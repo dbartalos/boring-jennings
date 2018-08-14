ab8d1a3c5cca813b887227868e5a3bc5d7877ce4
dbartalos <dbartalos@NANDOS-LT1113>
2018-08-15 00:19:38 +0100

    Example project with Travis CI

---
+++ exampleproject/hello.py
@@ -0,0 +1,9 @@
+"""
+This is a sample module docstring
+"""
+
+
+def hello(text_input):
+    """ This is a sample function docstring """
+    msg = text_input
+    return msg
---
+++ exampleproject1/hello.py
@@ -0,0 +1,9 @@
+"""
+This is a sample module
+"""
+
+
+def hello(text_input):
+    """ This is a sample function """
+    msg = text_input
+    return msg
